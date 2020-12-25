92f286ed2786c8d93a921d891dd1bd7a30b9d123
jpcoding <jiaopu94@hotmail.com>
2020-12-25 15:51:47 -0600

    update kilo.c

--- editor/kilo.c
+++ editor/kilo.c
@@ -1,7 +1,20 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
-
+struct termios orig_termios;
+void disableRawMode() { … }
+void enableRawMode() { … }
 int main() {
-    char c;
-    while (read(STDIN_FILENO,&c, 1) ==1 && c !='q');
-    return 0;
+  enableRawMode();
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
+  return 0;
 }
