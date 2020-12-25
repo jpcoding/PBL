ff3c290ba5c0f133d0db657cf5655a44513ff8aa
jpcoding <jiaopu94@hotmail.com>
2020-12-25 15:56:05 -0600

    update kilo.c

--- editor/kilo.c
+++ editor/kilo.c
@@ -3,9 +3,21 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
 struct termios orig_termios;
-void disableRawMode() { … }
-void enableRawMode() { … }
+
+void disableRawMode() { 
+      tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+ }
+
+void enableRawMode() { 
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+ }
+
 int main() {
   enableRawMode();
   char c;
