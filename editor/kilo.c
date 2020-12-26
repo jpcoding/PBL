a07ad6ee3c6cffd5234b7352d9e1268398538739
jpcoding <jiaopu94@hotmail.com>
2020-12-26 16:24:08 -0600

    editor update

--- editor/kilo.c
+++ editor/kilo.c
@@ -1,52 +1,108 @@
+/*** includes ***/
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
-struct termios orig_termios;
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 
-void disableRawMode(){
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)==-1)    
-    die("tcsetattr");
-}
+/*** data  ***/
+struct editorConfig {
+  struct termios orig_termios;
+};
+struct editorConfig E;
 
-void enableRawMode() { 
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)==-1) die("tcsetattr");
-
-    atexit(disableRawMode);
-    struct termios raw = orig_termios;
-    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    raw.c_oflag &= ~(OPOST);
-    raw.c_cflag |= (CS8);
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); 
-    raw.c_cc[VMIN] = 0;
-    raw.c_cc[VTIME] = 10;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
-
- }
-
+/*** terminal  ***/
  void die (const char *s){
+  write (STDOUT_FILENO, "\x1b[2]", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
    perror(s);
    exit(1);
  }
 
+void disableRawMode(){
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+    die("tcsetattr");
+}
 
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
+  atexit(disableRawMode);
+  struct termios raw = E.orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+ char editorReadKey(){
+   int nread;
+   char c;
+   while ((nread = read(STDERR_FILENO, &c, 1))!= 1 ) {
+     if( nread ==-1 && errno != EAGAIN) die("read");
+   }
+   return c;
+ }
+
+int getWindowsSize (int *rows, int *cols) {
+  struct  winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) ==-1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+  {
+    /* data */
+  };
+  
+}
+
+ /*** input  ***/
+ void editorProcessKeypress (){
+   char c = editorReadKey();
+   switch (c) {
+     case CTRL_KEY('q'):
+      write (STDOUT_FILENO, "\x1b[2]", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+   }
+ }
+
+/*** output ***/
+void editorDrawRows(){
+  int y;
+  for (y=0; y<24; y++){
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+void editorRefreshScreen() {
+  write (STDOUT_FILENO, "\x1b[2]", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  
+}
+
+/*** init ***/
 int main() {
   enableRawMode();
 
   while (1){
-    char c = '\0';
-    if(read(STDIN_FILENO, &c, 1)==-1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)) {
-      printf("%d\n", c);
-    } else {
-      printf("%d ('%c')\n", c, c);
-    }
-    if (c =='q') break;
+   editorRefreshScreen();
+   editorProcessKeypress();
   }
 
   return 0;
